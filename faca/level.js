3d4e341645b0092dee0f24bd7c635f48409dc015
Diego Viniegra <diegovillalogos@catedra.edu.co>
2018-12-07 15:51:54 -0500

    Crea juego demo faca

---
+++ faca/level.js
@@ -0,0 +1,33 @@
+
+
+window._world = {
+  "viewportBottom": 156,
+  "name": "",
+  "x": 0,
+  "y": 0,
+  "tileWidth": 32,
+  "tileHeight": 32,
+  "width": 30,
+  "height": 17,
+  "viewportTop": 0,
+  "viewportRight": 0,
+  "viewportLeft": 0,
+  "backgroundColor": "rgba(66, 66, 255, 1)",
+  "sprites": [
+    {
+      "name": "pennie",
+      "state": "idle",
+      "x": 192,
+      "y": 416
+    },
+    {
+      "name": "brick-top",
+      "state": "idle",
+      "sequenceIndex": 0,
+      "x": 142,
+      "y": 316
+    }
+  ],
+  "state": "edit",
+  "time": 0
+};
